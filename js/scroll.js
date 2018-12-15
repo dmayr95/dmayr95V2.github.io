47941230e371e112a8bc940d9fd87460918ec24c
damir <damirmarkovic13@yahoo.com>
2018-12-16 00:02:49 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,40 @@
-var position = 400;
+let position = 400;
+let screenWidth;
+let hilfsvar = 0;
+
+
+function getScreenWidth() {
+    screenWidth = window.innerWidth;
+}
 
 function scrollpos() {
-    var elemnt = document.getElementById("body");
-    var top = window.scrollY;
-    console.log(top);
+    getScreenWidth();
+    let top = window.scrollY;
+    let navbar = document.getElementById("nav-bar");
+    let links = document.getElementById("nav-links");
 
-    var navbar = document.getElementById("nav-bar");
-    var links = document.getElementById("nav-links");
-
-    if(top >= position){
-        navbar.style.cssText = "width:230px;}";
-        links.style.cssText = "display:none;";
+    if(screenWidth > 700){
+        if(top >= position){
+            navbar.style.cssText = "width:230px;";
+            links.style.cssText = "display:none;";
+        }
+        else if(top < position){
+            navbar.style.cssText = "width:100%;";
+            links.style.cssText = "display:grid;";
+        }
     }
-    else if(top < position){
-        navbar.style.cssText = "width:100%;}";
-        links.style.cssText = "display:grid; ";
-    }
+}
 
+function showMenu(){
+    let menuContainer = document.getElementById("nav-bar");
+
+    if(hilfsvar === 0){
+        menuContainer.style.cssText = "margin-left:0px !important;";
+        hilfsvar = 1;
+    }
+    else{
+        menuContainer.style.cssText = "margin-left:-180px !important;";
+        hilfsvar = 0;
+    }
 }
 
