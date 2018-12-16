91a283708cd0bd172899c152f0a2800b33b94601
damir <damirmarkovic13@yahoo.com>
2018-12-16 01:16:58 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,5 @@
 let position = 400;
 let screenWidth;
-let hilfsvar = 0;
 
 
 function getScreenWidth() {
@@ -25,25 +24,4 @@ function scrollpos() {
     }
 }
 
-function showMenu(){
-    let menuContainer = document.getElementById("nav-bar");
-    let firstButtonLine = document.getElementById("first-button-line");
-    let secondButtonLine = document.getElementById("second-button-line");
-    let lastButtonLine = document.getElementById("third-button-line");
-
-    if(hilfsvar === 0){
-        menuContainer.style.cssText = "margin-left:0px !important;";
-        hilfsvar = 1;
-        firstButtonLine.style.cssText = "transform: rotate(45deg); margin-top:15px;";
-        lastButtonLine.style.cssText = "transform: rotate(-45deg); margin-top:-25px;";
-        secondButtonLine.style.cssText = "visibility: hidden;";
-    }
-    else{
-        menuContainer.style.cssText = "margin-left:-180px !important;";
-        hilfsvar = 0;
-        firstButtonLine.style.cssText = "transform: rotate(0deg);";
-        lastButtonLine.style.cssText = "transform: rotate(0deg);";
-        secondButtonLine.style.cssText = "visibility: visible;";
-    }
-}
 
