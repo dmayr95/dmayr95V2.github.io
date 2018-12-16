92305784aa317be2891b9e74ad9f05414bb866b5
damir <damirmarkovic13@yahoo.com>
2018-12-16 01:04:34 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -27,14 +27,23 @@ function scrollpos() {
 
 function showMenu(){
     let menuContainer = document.getElementById("nav-bar");
+    let firstButtonLine = document.getElementById("first-button-line");
+    let secondButtonLine = document.getElementById("second-button-line");
+    let lastButtonLine = document.getElementById("third-button-line");
 
     if(hilfsvar === 0){
         menuContainer.style.cssText = "margin-left:0px !important;";
         hilfsvar = 1;
+        firstButtonLine.style.cssText = "transform: rotate(45deg); margin-top:15px;";
+        lastButtonLine.style.cssText = "transform: rotate(-45deg); margin-top:-25px;";
+        secondButtonLine.style.cssText = "visibility: hidden;";
     }
     else{
         menuContainer.style.cssText = "margin-left:-180px !important;";
         hilfsvar = 0;
+        firstButtonLine.style.cssText = "transform: rotate(0deg);";
+        lastButtonLine.style.cssText = "transform: rotate(0deg);";
+        secondButtonLine.style.cssText = "visibility: visible;";
     }
 }
 
