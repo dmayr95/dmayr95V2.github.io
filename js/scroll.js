1a63715c923b1292ce249f5b0e237ad90b9dd582
damir <damirmarkovic13@yahoo.com>
2018-12-22 14:51:00 +0100

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,31 @@
 let position = 400;
 let screenWidth;
 
-
 function getScreenWidth() {
     screenWidth = window.innerWidth;
+}
+
+function menuResize(){
+    getScreenWidth();
+    let top = window.scrollY;
+    let navbar = document.getElementById("nav-bar");
+    let links = document.getElementById("nav-links");
+
+    if(screenWidth < 850 && top >= position){
+        navbar.style.cssText = "width:180px;";
+        links.style.cssText = "display:grid;";
+    }
+    else if(screenWidth > 850 && top >= position){
+        navbar.style.cssText = "width:230px;";
+        links.style.cssText = "display:none;";
+    }
+    else if(screenWidth < 850  && top <= position){
+        navbar.style.cssText = "width:180px; margin-left:-180px;";
+    }
+    else if(screenWidth > 850  && top <= position){
+        navbar.style.cssText = "width:100%; margin-left:0px;"
+    }
+
 }
 
 function scrollpos() {
@@ -23,5 +45,4 @@ function scrollpos() {
         }
     }
 }
-
 
